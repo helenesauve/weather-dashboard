f79e5f690352f00391cc137cfc873af7765bc443
Hélène Sauvé <helenesauve@Helenes-MBP.lan>
2023-01-28 18:48:52 +0000

    adding temp, wind and humidity for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,30 @@ $("#search-button").on("click", function (event) {
   }).then(function (response) {
     console.log(response);
     // city name
-    var cityArea = response.city;
+    var cityArea = response.city.name;
+    cityArea = $("<div>");
+    cityArea.text(city);
+    
+    var today = $("#today");
+    var forecast = $("#forecast");
+    today.append("<h2>" + city + " " + moment().format("MMMM Do YYYY") + "</h2>");
+
+    // adding wind on the page
+    var wind = response.list[0].wind.speed;
+    // wind = $("<div>");
+
+    // adding temperature on the page
+    var temperature = (response.list[0].main.temp);
+    console.log(response.list[0].main.temp)
+
+
+    var humidity = (response.list[0].main.humidity);
+
+    today.append("<h3>" + "Temp: " + temperature +  " kelvin" + "</h3>")
+    today.append("<h3>" + "Wind: "+ wind + "KPH" + "</h3>")
+    today.append("<h3>" + "Humidity: " + humidity + "%" + "</h3>")
+    // today.append("<h3>" + "Humidity:" + JSON.stringify(humidity) + "</h3>")
+   
     // creating array with the 5 next days
     var days = [
       (day1 = response.list[0].dt_txt),
@@ -23,21 +46,17 @@ $("#search-button").on("click", function (event) {
     //   (day6 = response.list[40].dt_txt)
     ];
 
-    cityArea = $("<div>");
-    cityArea.text(city);
-    var today = $("#today");
-    today.append(cityArea);
-    //selecting div today
-    $("section#today").addClass("mt-3");
+    console.log(response.list[0].wind)
 
     //for each day, display day using moment.js
     //display temp, wind, humidity
     for (var i = i; i < days.length; i++) {
-      var wind = response.list[i].wind;
-      var temperature = response.list[i].main[1];
-      var forecast = $("#forecast");
-      forecast.append(wind);
-      forecast.append(temperature);
+        var wind = response.list[i].wind;
+        var temperature = response.list[i].main.temp;
+
+      
+    //   forecast.append(wind);
+    //   forecast.append(temperature);
     }
   });
 });
