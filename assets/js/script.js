09cc97329e3f1ad4eeb51434cbc8645c13bb6649
Hélène Sauvé <helenesauve@Helenes-MBP.lan>
2023-01-28 15:12:30 +0000

    selecting the div today and applying style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,46 @@
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
-    var city = $("#search-input").val();
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=b8373b3fe2c318bbddad2464aefa9025";
-  
+$("#search-button").on("click", function (event) {
+  event.preventDefault();
+  var city = $("#search-input").val();
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=b8373b3fe2c318bbddad2464aefa9025";
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-      })
-        .then(function(response) {
-            console.log(response)
-            // city name
-            var cityArea = response.city;
-            // creating array with the 5 next days
-            var days = [
-            day1 = response.list[0].dt_txt,
-            day2 = response.list[8].dt_txt,
-            day3 = response.list[16].dt_txt,
-            day4 = response.list[24].dt_txt,
-            day5 = response.list[32].dt_txt
-            ]
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // city name
+    var cityArea = response.city;
+    // creating array with the 5 next days
+    var days = [
+      (day1 = response.list[0].dt_txt),
+      (day2 = response.list[8].dt_txt),
+      (day3 = response.list[16].dt_txt),
+      (day4 = response.list[24].dt_txt),
+      (day5 = response.list[32].dt_txt),
+    //   (day6 = response.list[40].dt_txt)
+    ];
 
-            for (var i = i; i < days.length; i++){
+    cityArea = $("<div>");
+    cityArea.text(city);
+    var today = $("#today");
+    today.append(cityArea);
+    //selecting div today
+    $("section#today").addClass("mt-3");
 
-            }
-            //var wind = response.list[i].wind
-
-            //var temperature = response.list[i].main[1];
-            
-            cityArea = $("<div>")
-            cityArea.text(city);
-            var today = $("#today")
-            today.append(cityArea)
-            
-            
-})
-})
-
-
+    //for each day, display day using moment.js
+    //display temp, wind, humidity
+    for (var i = i; i < days.length; i++) {
+      var wind = response.list[i].wind;
+      var temperature = response.list[i].main[1];
+      var forecast = $("#forecast");
+      forecast.append(wind);
+      forecast.append(temperature);
+    }
+  });
+});
 
 // date
 // icon representation
